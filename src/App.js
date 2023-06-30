849a785be4a677b0bdba139647924cbba59f44f4
Yasith Eranda Yapa Amarasekara <ya794120@gmail.com>
2023-06-30 19:57:24 +0530

    Main Banner Create

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import NavBar from './components/Navbar';
 import { BrowserRouter as Router } from 'react-router-dom';
+import headerImage from './images/headerImage.svg'
+import {FaArrowRight} from 'react-icons/fa';
 
 function App() {
   return (
@@ -8,8 +10,24 @@ function App() {
       <Router>
         <div className='heder'>
           <NavBar />
+          <div className='heder_cont'>
+            <div className='hederText'>
+
+              <h2>PUTTING YOU & YOURS TOGETHER</h2>
+              <h1 className='maintext'>BEFORE YOU BUILD</h1>
+              <p>BoQ providing a detailed fact of quantities, specifications, and costs of materials needed for a project.</p>
+              <button type='button' className='heder__button'>Let's Talk   <FaArrowRight /></button> 
+
+            </div>
+            <div className='heder-image'>
+
+              <img src={headerImage} alt={'heder'}/>
+              
+            </div>
+            </div>
           </div>
       </Router>
+      <img src={headerImage} alt={'heder'}/>
     </div>
   );
 }
