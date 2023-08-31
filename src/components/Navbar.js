b265ad06e46d60ebb05d4990a7427ae7106a83c2
Yasith Eranda Yapa Amarasekara <ya794120@gmail.com>
2023-09-01 02:39:04 +0530

    update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -102,7 +102,7 @@ export default function NavBar() {
             </Transition>
           </Popover>
 
-          <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+          <a href="boq-home" className="text-sm font-semibold leading-6 text-gray-900">
             BoQ Tool
           </a>
           <a href="/about" className="text-sm font-semibold leading-6 text-gray-900">
@@ -168,7 +168,7 @@ export default function NavBar() {
                   )}
                 </Disclosure>
                 <a
-                  href="#"
+                  href="/boq-tool"
                   className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                 >
                   BoQ Tool
