4dfae5a07058575d43525c3ea97fb575c5b84127
Yasith Eranda Yapa Amarasekara <ya794120@gmail.com>
2023-08-31 22:03:05 +0530

    add wall UI

--- src/boq_generator/walls/WallView.jsx
+++ src/boq_generator/walls/WallView.jsx
@@ -1,18 +1,115 @@
-import React from "react";
-import { Navbar } from "react-bootstrap";
-import NavBar from "../../components/Navbar";
+import React, { useState } from "react";
+import Navbar from "../../components/Navbar";
 import Footer from "../../components/Footer";
-import SingleInputFild from "../../components/SingleInputFiels";
+import axios from "axios";
 
 const WallView = () => {
-    return ( 
+  const [formData, setFormData] = useState({
+    height: "",
+    length: "",
+    unit: "ft",
+  });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post(
+        "http://localhost:8080/Models/Process/PartsOfConstructions/wall_process.php",
+        formData,
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      console.log("Response from PHP:", response.data);
+      if (response.data.message === "Data received successfully") {
+      }
+    } catch (error) {
+      console.error("Error submitting form:", error);
+    }
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  return (
     <div>
-        <NavBar/>
-    <SingleInputFild title="height" name="height" id="height" placeholder="enter wall width...."/>
-        <Footer/>
-        
-    </div> 
-    );
-}
- 
+      <Navbar />
+
+      <div className="bg-gray-100 min-h-screen flex items-center justify-center">
+        <div className="bg-white rounded p-8 shadow-md">
+        <h1 className="block text-sm font-medium text-xl text-gray-700 text-center pb-10">Add wall dimention</h1>
+
+          <form onSubmit={handleSubmit}>
+            <div className="mb-4">
+              <label
+                htmlFor="height"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Height
+              </label>
+              <input
+                type="text"
+                name="height"
+                value={formData.height}
+                onChange={handleInputChange}
+                className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+              />
+            </div>
+            <div className="mb-4">
+              <label
+                htmlFor="length"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Length
+              </label>
+              <input
+                type="text"
+                name="length"
+                value={formData.length}
+                onChange={handleInputChange}
+                className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+              />
+            </div>
+
+            <div className="mb-4">
+              <label
+                htmlFor="currency"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Unit
+              </label>
+              <select
+                name="unit"
+                value={formData.currency}
+                onChange={handleInputChange}
+                className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+              >
+                <option value="ft">ft</option>
+                <option value="m">m</option>
+              </select>
+            </div>
+
+            <button
+              type="submit"
+              className="w-full px-4 py-2 bg-indigo-600 text-white rounded-md hover:bg-indigo-500 focus:outline-none focus:bg-indigo-500"
+            >
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
 export default WallView;
--- src/components/SingleInputFiels.jsx
+++ src/components/SingleInputFiels.jsx
@@ -18,16 +18,15 @@ export default function SingleInputFild() {
           />
           <div className="absolute inset-y-0 right-0 flex items-center">
             <label htmlFor="currency" className="sr-only">
-              Currency
+              Units
             </label>
             <select
               id="currency"
               name="currency"
               className="h-full rounded-md border-0 bg-transparent py-0 pl-2 pr-7 text-gray-500 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm"
             >
-              <option>USD</option>
-              <option>CAD</option>
-              <option>EUR</option>
+              <option>ft</option>
+              <option>m</option>
             </select>
           </div>
         </div>
