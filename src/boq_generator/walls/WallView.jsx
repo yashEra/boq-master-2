299bc9565d86b13bcb2859799cbe46e8f795df35
Yasith Eranda Yapa Amarasekara <ya794120@gmail.com>
2023-09-02 15:44:17 +0530

    Walls Functions Working Correctly

--- src/boq_generator/walls/WallView.jsx
+++ src/boq_generator/walls/WallView.jsx
@@ -13,6 +13,15 @@ const WallView = () => {
     brickTypes: "Clay Brick"
   });
 
+  const [showDataSection, setShowDataSection] = useState(false);
+
+  const [data, setData] = useState({
+    noOfBricks: null,
+    sandQ: null,
+    cemntQ: null,
+    totalCost: null
+  });
+
   const handleSubmit = async (e) => {
     e.preventDefault();
 
@@ -28,7 +37,16 @@ const WallView = () => {
       );
 
       console.log("Response from PHP:", response.data);
+      console.log("Response from PHP:", response.data.numberOfBricks);
+      
       if (response.data.message === "Data received successfully") {
+        setData({
+          noOfBricks: response.data.numberOfBricks,
+          sandQ: response.data.Sand,
+          cemntQ: response.data.CementKg,
+          totalCost: response.data.cost
+        });
+        setShowDataSection(true);
       }
     } catch (error) {
       console.error("Error submitting form:", error);
@@ -48,85 +66,94 @@ const WallView = () => {
       <Navbar />
 
       <div className=" min-h-screen flex items-center justify-center" style={{ backgroundImage: `url('${backgroundImageUrl}')` }}>
-        <div className="bg-white rounded p-16 shadow-md">
-        <h1 className="block text-sm font-medium text-xl text-gray-700 text-center pb-10">Add wall dimention</h1>
+        {showDataSection ? (
+          <div className="bg-white rounded p-16 shadow-md">
+            <h1 className="block text-sm font-medium text-xl text-gray-700 text-center pb-10">Data Received</h1>
+            <p>No of Bricks: {data.noOfBricks}</p>
+            <p>Sand Quantity: {data.sandQ}</p>
+            <p>Cement Quantity: {data.cemntQ}</p>
+            <p>Total Cost: {data.totalCost}</p>
+          </div>
+        ) : (
+          <div className="bg-white rounded p-16 shadow-md">
+            <h1 className="block text-sm font-medium text-xl text-gray-700 text-center pb-10">Add wall dimension</h1>
+            <form onSubmit={handleSubmit}>
+              <div className="mb-4">
+                <label
+                  htmlFor="height"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  Height
+                </label>
+                <input
+                  type="text"
+                  name="height"
+                  value={formData.height}
+                  onChange={handleInputChange}
+                  className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                />
+              </div>
+              <div className="mb-4">
+                <label
+                  htmlFor="length"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  Length
+                </label>
+                <input
+                  type="text"
+                  name="length"
+                  value={formData.length}
+                  onChange={handleInputChange}
+                  className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                />
+              </div>
 
-          <form onSubmit={handleSubmit}>
-            <div className="mb-4">
-              <label
-                htmlFor="height"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Height
-              </label>
-              <input
-                type="text"
-                name="height"
-                value={formData.height}
-                onChange={handleInputChange}
-                className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-              />
-            </div>
-            <div className="mb-4">
-              <label
-                htmlFor="length"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Length
-              </label>
-              <input
-                type="text"
-                name="length"
-                value={formData.length}
-                onChange={handleInputChange}
-                className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-              />
-            </div>
+              <div className="mb-4">
+                <label
+                  htmlFor="unit"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  Select Unit
+                </label>
+                <select
+                  name="unit"
+                  value={formData.unit}
+                  onChange={handleInputChange}
+                  className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                >
+                  <option value="ft">ft</option>
+                  <option value="m">m</option>
+                </select>
+              </div>
 
-            <div className="mb-4">
-              <label
-                htmlFor="unit"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Select Unit
-              </label>
-              <select
-                name="unit"
-                value={formData.unit}
-                onChange={handleInputChange}
-                className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-              >
-                <option value="ft">ft</option>
-                <option value="m">m</option>
-              </select>
-            </div>
+              <div className="mb-4">
+                <label
+                  htmlFor="brickTypes"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  Select Brick Type
+                </label>
+                <select
+                  name="brickTypes"
+                  value={formData.brickTypes}
+                  onChange={handleInputChange}
+                  className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                >
+                  <option value="clayBrick">Clay Brick</option>
+                  <option value="cementBrick">Cement Brick</option>
+                </select>
+              </div>
 
-            <div className="mb-4">
-              <label
-                htmlFor="brickTypes"
-                className="block text-sm font-medium text-gray-700"
+              <button
+                type="submit"
+                className="w-full px-4 py-2 bg-indigo-600 text-white rounded-md hover:bg-indigo-500 focus:outline-none focus:bg-indigo-500"
               >
-                Select Brick Type
-              </label>
-              <select
-                name="brickTypes"
-                value={formData.brickTypes}
-                onChange={handleInputChange}
-                className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-              >
-                <option value="clayBrick">Clay Brick</option>
-                <option value="cementBrick">Cement Brick</option>
-              </select>
-            </div>
-
-            <button
-              type="submit"
-              className="w-full px-4 py-2 bg-indigo-600 text-white rounded-md hover:bg-indigo-500 focus:outline-none focus:bg-indigo-500"
-            >
-              Submit
-            </button>
-          </form>
-        </div>
+                Submit
+              </button>
+            </form>
+          </div>
+        )}
       </div>
       <Footer />
     </div>
